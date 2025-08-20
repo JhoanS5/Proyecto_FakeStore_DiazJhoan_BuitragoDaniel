c6e4f87372db1321dad38eb973a35a646d229684
Jhoan Sebastian Diaz Balta <jhoan.diaz@ias.com.co>
2025-08-20 16:54:41 -0500

    feat: Integre inicialización del carrito en main.js

--- modules/main.js
+++ modules/main.js
@@ -1,10 +1,11 @@
 import { loadProductos } from "./api/api.js";
 import { inicializarFiltros } from "./ui/filtros.js";
+import { inicializarCarritoUI } from "./ui/carritoUI.js";
 
 async function main() {
     const productos = await loadProductos();
-    
     inicializarFiltros(productos);
+    inicializarCarritoUI();
 }
 
 main();
